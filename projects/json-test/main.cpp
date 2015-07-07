b828ac4555192700cb14ea2b71cb7cda4f8498cd
Jeff Wang <wohaaitinciu@163.com>
2015-07-08 01:15:04 +0800

    整理测试代码

--- projects/json-test/main.cpp
+++ projects/json-test/main.cpp
@@ -23,12 +23,6 @@ template <class _MAP> void func2(const _MAP &map) {
     std::cout << std::endl;
 }
 
-template <template <class> class T, template <class> class A>
-struct Test {
-    std::basic_string<char, T<char>, A<char> > str;
-    std::char_traits<char> a;
-};
-
 struct TestCharTraits {
     typedef char _Elem;
     typedef _Elem char_type;
@@ -49,16 +43,16 @@ struct TestCharTraits {
     static _Elem *copy(_Elem *_First1, const _Elem *_First2, size_t _Count) {	// copy [_First2, _First2 + _Count) to [_First1, ...)
         return (_Count == 0 ? _First1 : (_Elem *)memcpy(_First1, _First2, _Count));
     }
-    /*
-    static _Elem *_Copy_s(
-        _Out_writes_(_Size_in_bytes) _Elem *_First1, size_t _Size_in_bytes,
-        _In_reads_(_Count) const _Elem *_First2, size_t _Count)
-    {	// copy [_First2, _First2 + _Count) to [_First1, ...)
-        if (0 < _Count)
-            _CRT_SECURE_MEMCPY(_First1, _Size_in_bytes, _First2, _Count);
-        return (_First1);
-    }
-    */
+
+    //static _Elem *_Copy_s(
+    //    _Out_writes_(_Size_in_bytes) _Elem *_First1, size_t _Size_in_bytes,
+    //    _In_reads_(_Count) const _Elem *_First2, size_t _Count)
+    //{	// copy [_First2, _First2 + _Count) to [_First1, ...)
+    //    if (0 < _Count)
+    //        _CRT_SECURE_MEMCPY(_First1, _Size_in_bytes, _First2, _Count);
+    //    return (_First1);
+    //}
+
     static const _Elem * find(const _Elem *_First, size_t _Count, const _Elem& _Ch) {	// look for _Ch in [_First, _First + _Count)
         return (_Count == 0 ? (const _Elem *)0 : (const _Elem *)memchr(_First, _Ch, _Count));
     }
@@ -257,105 +251,76 @@ int main(int argc, char *argv[])
 	auto initList = { "1", "2", "3", "c" };
 	std::cout << cppJSON(initList) << std::endl;
 
+    std::cout << "==========Construct From Map==========" << std::endl;
+    std::cout << cppJSON(std::map<const char *, int>({ { "a", 1 }, { "b", 2 }, { "c", 3 }, { "d", 4 }, { "e", 5 } })) << std::endl;
+    std::cout << cppJSON(std::multimap<cppJSON::StringType, int>({ { "a", 1 }, { "b", 2 }, { "c", 3 }, { "d", 4 }, { "e", 5 } })) << std::endl;
+    std::cout << cppJSON(std::unordered_map<std::string, int>({ { "a", 1 }, { "b", 2 }, { "c", 3 }, { "d", 4 }, { "e", 5 } })) << std::endl;
 	// TODO:
 	//std::wcout << js5.as<std::wstring>();// << std::endl;
 
 	std::cout << "==========Construct From Array End==========" << std::endl;
-	{
-        cppJSON jv = cppJSON(std::vector<int>({ 1, 2, 3, 4, 5, 6, 7 }));
-        cppJSON jvt = jv;
 
-        cppJSON jvtt = std::move(jvt);
+    std::cout << "==========Parse Array==========" << std::endl;
+    cppJSON json;
+    json.Parse("[\"Sunday\", \"Monday\", \"Tuesday\", \"Wednesday\", \"Thursday\", \"Friday\", \"Saturday\"]");
+    std::cout << json.stringfiy() << std::endl;
+    std::cout << "==========As Array==========" << std::endl;
+    func(json.as<std::vector<std::string> >());
+    func(json.as<std::list<std::string> >());
+    func(json.as<std::set<std::string> >());
+    func(json.as<std::multiset<std::string> >());
+    func(json.as<std::unordered_set<std::string> >());
+    func(json.as<std::unordered_multiset<std::string> >());
+    //json.clear();
 
-        std::map<std::string, int> bbb;
-        std::transform(jv.begin(), jv.end(),
-            std::insert_iterator<std::map<std::string, int> >(bbb, bbb.begin()), //[](const int &j) { return std::make_pair(0, 0); });//std::bind(&cppJSON::as<int>, std::placeholders::_1));
-            std::bind(&std::make_pair<std::string, int>, "abc", std::bind(&cppJSON::as<int>, std::placeholders::_1)));
-            //[](const cppJSON &j) { return std::make_pair("abc", j.as<int>()); });
+    std::cout << "==========Parse Map==========" << std::endl;
+    json.Parse("{\"name2\":\"Jack\",\"name3\":\"Jack\",\"name4\":\"Jack\",\"name5\":\"Jack\",\"name6\":\"Jack\",\"name7\":\"Jack\"}");
+    std::cout << json << std::endl;
+    std::cout << "==========As Map==========" << std::endl;
+    func2(json.as<std::map<std::string, std::string> >());
+    func2(json.as<std::multimap<std::string, std::string> >());
+    func2(json.as<std::unordered_map<std::string, std::string> >());
+    func2(json.as<std::unordered_multimap<std::string, std::string> >());
 
-        std::for_each(jv.begin(), jv.end(), [](const cppJSON &j) {
-            std::cout << j.Print() << std::endl;
-        });
-        //std::for_each(jv.cbegin(), jv.cend(), std::ostream_iterator<const cppJSON>(std::cout, ", "));
+    std::cout << "==========Parse==========" << std::endl;
+    json.Parse("{\n\"name\": \"Jack (\\\"Bee\\\") Nimble\", \n\"format\": {\"type\":       \"rect\", \n\"width\":      1920, \n\"height\":     1080, \n\"interlace\":  false,\"frame rate\": 24\n}\n}");
+    std::cout << json << std::endl;
+    //func2(js1.as<std::map<std::string, JSON_Value> >());
 
-        std::for_each(jv.rbegin(), jv.rend(), [](const cppJSON &j) {
-            std::cout << j.Print() << std::endl;
-        });
-        std::cout << jv.Print() << std::endl;
-
-        jv.Parse("[\"Sunday\", \"Monday\", \"Tuesday\", \"Wednesday\", \"Thursday\", \"Friday\", \"Saturday\"]");
-        std::cout << jv.Print() << std::endl;
-        func(jv.as<std::vector<std::string> >());
-        func(jv.as<std::list<std::string> >());
-        func(jv.as<std::set<std::string> >());
-        func(jv.as<std::multiset<std::string> >());
-        func(jv.as<std::unordered_set<std::string> >());
-        func(jv.as<std::unordered_multiset<std::string> >());
-        jv.clear();
-
-        jv.Parse("{\"name2\":\"Jack\",\"name3\":\"Jack\",\"name4\":\"Jack\",\"name5\":\"Jack\",\"name6\":\"Jack\",\"name7\":\"Jack\"}");
-        std::cout << jv.Print() << std::endl;
-
-        func2(jv.as<std::map<std::string, std::string> >());
-        func2(jv.as<std::multimap<std::string, std::string> >());
-        func2(jv.as<std::unordered_map<std::string, std::string> >());
-        func2(jv.as<std::unordered_multimap<std::string, std::string> >());
-
-        std::unordered_map<int, int> t;
-        t.insert(std::unordered_map<int, int>::value_type(1, 1));
-    }
     //return 0;
     //using jw::JSON_Value;
     typedef cppJSON JSON_Value;
 
-    std::cout << "==========TEST ARRAY AS==========" << std::endl;
-    JSON_Value js;
-    js.Parse("[\"Sunday\", \"Monday\", \"Tuesday\", \"Wednesday\", \"Thursday\", \"Friday\", \"Saturday\"]");
-    std::cout << js << std::endl;
-
-    func(js.as<std::vector<std::string> >());
-    func(js.as<std::list<std::string> >());
-    func(js.as<std::set<std::string> >());
-    func(js.as<std::multiset<std::string> >());
-    func(js.as<std::unordered_set<std::string> >());
-    func(js.as<std::unordered_multiset<std::string> >());
-
-    std::cout << "==========TEST OBJECT==========" << std::endl;
-    JSON_Value js1;
-    js1.Parse("{\n\"name\": \"Jack (\\\"Bee\\\") Nimble\", \n\"format\": {\"type\":       \"rect\", \n\"width\":      1920, \n\"height\":     1080, \n\"interlace\":  false,\"frame rate\": 24\n}\n}");
-    std::cout << js1 << std::endl;
-    //func2(js1.as<std::map<std::string, JSON_Value> >());
-
-    std::cout << "==========TEST OBJECT MERGE==========" << std::endl;
-    JSON_Value temp;
-    temp.Parse("{\"name2\":\"Jack\",\"name3\":\"Jack\",\"name4\":\"Jack\",\"name5\":\"Jack\",\"name6\":\"Jack\",\"name7\":\"Jack\"}");
+    //std::cout << "==========TEST OBJECT MERGE==========" << std::endl;
+    //JSON_Value temp;
+    //temp.Parse("{\"name2\":\"Jack\",\"name3\":\"Jack\",\"name4\":\"Jack\",\"name5\":\"Jack\",\"name6\":\"Jack\",\"name7\":\"Jack\"}");
     //js1.merge(temp);
-    std::cout << js1 << std::endl;
+    //std::cout << js1 << std::endl;
     //func2(js1.as<std::map<std::string, JSON_Value> >());
 
-    std::cout << "==========TEST ARRAY MERGE==========" << std::endl;
-    JSON_Value js8({ "abc" });
+    //std::cout << "==========TEST ARRAY MERGE==========" << std::endl;
+    //JSON_Value js8({ "abc" });
     //js8.merge(js7);
-    std::cout << js8 << std::endl;
+    //std::cout << js8 << std::endl;
 
-    std::cout << "==========TEST OBJECT CONSTRUCT==========" << std::endl;
+    //std::cout << "==========TEST OBJECT CONSTRUCT==========" << std::endl;
     //auto bbb = std::map<std::string, JSON_Value>({{ "1", JSON_Value(1) }, { "2", JSON_Value(2) }, { "3", JSON_Value(3) }});
     //JSON_Value js9(bbb);
     //std::cout << js9 << std::endl;
 
-    std::cout << "==========TEST OBJECT CONSTRUCT(VS2013 BUG)==========" << std::endl;
+    //std::cout << "==========TEST OBJECT CONSTRUCT(VS2013 BUG)==========" << std::endl;
     //auto ccc = std::initializer_list<std::pair<const char*, JSON_Value> >({ { "ab", JSON_Value(1) }, { "b13", JSON_Value("b") }, { "c", JSON_Value(0.5F) } });
     //JSON_Value js10(ccc);  // 这里是VS2013的BUG
     //std::cout << js10 << std::endl;
 
-    std::cout << "==========TEST OBJECT CONSTRUCT==========" << std::endl;
+    //std::cout << "==========TEST OBJECT CONSTRUCT==========" << std::endl;
     JSON_Value js11;
     //js11.insert("123", JSON_Value("123"));
     //js11.insert("456", JSON_Value({1, 2, 3, 4, 5, 6, 7}));
     //js11.insert("789", JSON_Value(0.5F));
-    std::cout << js11 << std::endl;
+    //std::cout << js11 << std::endl;
 
-    std::cout << "==========TEST OBEJCT AS==========" << std::endl;
+    //std::cout << "==========TEST OBEJCT AS==========" << std::endl;
     //auto data11 = js11.as<std::unordered_map<std::string, JSON_Value> >();
     //auto it = data11.find("123");
     //if (it != data11.end())
@@ -364,6 +329,36 @@ int main(int argc, char *argv[])
     //}
 
     std::cout << (js11 == nullptr)  << (js11 != nullptr) << (nullptr == js11) << (nullptr != js11) << std::endl;
+
+    std::cout << "==========Iterators==========" << std::endl;
+    {
+        cppJSON jv = cppJSON(std::vector<int>({ 1, 2, 3, 4, 5, 6, 7 }));
+        std::cout << jv << std::endl;
+
+        cppJSON jvt = jv;
+
+        cppJSON jvtt = std::move(jvt);
+
+        //std::map<std::string, int> bbb;
+        //std::transform(jv.begin(), jv.end(), std::insert_iterator<std::map<std::string, int> >(bbb, bbb.begin()),
+        //    std::bind(&std::make_pair<std::string, int>, "abc", std::bind(&cppJSON::as<int>, std::placeholders::_1)));
+
+        std::cout << "==========Iterator==========" << std::endl;
+        std::for_each(jv.begin(), jv.end(), [](const cppJSON &j) {
+            std::cout << j << "  ";
+        });
+        std::cout << std::endl;
+
+        std::cout << "==========Const Iterator==========" << std::endl;
+        std::copy(jv.cbegin(), jv.cend(), std::ostream_iterator<const cppJSON &>(std::cout, ", "));
+        std::cout << std::endl;
+
+        std::cout << "==========Reverse Iterator==========" << std::endl;
+        std::for_each(jv.rbegin(), jv.rend(), [](const cppJSON &j) {
+            std::cout << j << "  ";
+        });
+        std::cout << std::endl;
+    }
 
     return 0;
 }
