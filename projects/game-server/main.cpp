36c0d47dc43e4222a60391570f0faef98114d47a
Jeff Wang <wohaaitinciu@163.com>
2015-07-26 18:26:17 +0800

    改成同时多个accept

--- projects/game-server/main.cpp
+++ projects/game-server/main.cpp
@@ -12,45 +12,36 @@
 
 namespace echo {
 
-    class Session
-    {
+    class Session : public std::enable_shared_from_this<Session> {
     public:
-        Session(asio::ip::tcp::socket &&socket) : _socket(std::move(socket))
-        {
+        Session(asio::ip::tcp::socket &&socket) : _socket(std::move(socket)) {
         }
 
-        void start()
-        {
+        ~Session() {
+            printf("Session::~Session");
+        }
+
+        void start() {
             _doRead();
         }
 
     private:
-        void _doRead()
-        {
+        void _doRead() {
+            auto thiz = shared_from_this();
             _socket.async_read_some(asio::buffer(_data, max_length),
-                [this](std::error_code ec, size_t length) {
-                if (!ec)
-                {
+                [this, thiz](std::error_code ec, size_t length) {
+                if (!ec) {
                     _doWrite(length);
-                }
-                else
-                {
-                    delete this;
                 }
             });
         }
 
-        void _doWrite(size_t length)
-        {
+        void _doWrite(size_t length) {
+            auto thiz = shared_from_this();
             asio::async_write(_socket, asio::buffer(_data, length),
-                [this](std::error_code ec, size_t length) {
-                if (!ec)
-                {
+                [this, thiz](std::error_code ec, size_t length) {
+                if (!ec) {
                     _doRead();
-                }
-                else
-                {
-                    delete this;
                 }
             });
         }
@@ -61,27 +52,19 @@ namespace echo {
         char _data[max_length];
     };
 
-    class Server
-    {
+    class Server {
     public:
         Server(asio::io_service &service, unsigned short port)
             : _acceptor(service, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port)),
-            _socket(service)
-        {
+            _socket(service) {
             _doAccept();
         }
 
     private:
-        void _doAccept()
-        {
+        void _doAccept() {
             _acceptor.async_accept(_socket, [this](std::error_code ec) {
-                if (!ec)
-                {
-                    Session *s = new (std::nothrow) Session(std::move(_socket));
-                    if (s != nullptr)
-                    {
-                        s->start();
-                    }
+                if (!ec) {
+                    std::make_shared<Session>(std::move(_socket))->start();
                 }
                 _doAccept();
             });
@@ -99,58 +82,48 @@ namespace echo {
 
 namespace chat {
 
-    class Session
-    {
+    class Session {
     public:
         Session(asio::ip::tcp::socket &&socket, std::function<void (Session *, const char *, size_t)> &&callback)
             : _socket(std::move(socket))
-            , _callback(callback)
-        {
+            , _callback(callback) {
         }
 
-        void start()
-        {
+        ~Session() {
+            printf("Session::~Session");
+        }
+
+        void start() {
             _doRead();
         }
 
-        void deliver(const char *data, size_t length)
-        {
+        void deliver(const char *data, size_t length) {
             bool empty = _writeQueue.empty();
             _writeQueue.push_back(asio::buffer(data, length));
-            if (empty)
-            {
+            if (empty) {
                 asio::async_write(_socket, _writeQueue.front(), std::bind(&Session::_doWrite, this, std::placeholders::_1, std::placeholders::_2));
             }
         }
 
     private:
-        void _doRead()
-        {
+        void _doRead() {
             _socket.async_read_some(asio::buffer(_readData, max_length), [this](std::error_code ec, size_t length) {
-                if (!ec)
-                {
+                if (!ec) {
                     _callback(this, _readData, length);
                     _doRead();
-                }
-                else
-                {
+                } else {
                     _callback(this, nullptr, 0);
                 }
             });
         }
 
-        void _doWrite(std::error_code ec, size_t length)
-        {
-            if (!ec)
-            {
+        void _doWrite(std::error_code ec, size_t length) {
+            if (!ec) {
                 _writeQueue.pop_front();
-                if (!_writeQueue.empty())
-                {
+                if (!_writeQueue.empty()) {
                     asio::async_write(_socket, _writeQueue.front(), std::bind(&Session::_doWrite, this, std::placeholders::_1, std::placeholders::_2));
                 }
-            }
-            else
-            {
+            } else {
                 _callback(this, nullptr, 0);
             }
         }
@@ -164,53 +137,56 @@ namespace chat {
         std::function<void (Session *, const char *, size_t)> _callback;
     };
 
-    class Server
-    {
+    class Server {
     public:
         Server(asio::io_service &service, unsigned short port)
-            : _acceptor(service, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port)),
-            _socket(service)
-        {
+            : _acceptor(service, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port)) {
+            for (size_t i = 0; i < max_accept; ++i) {
+                _socket[i] = std::make_shared<asio::ip::tcp::socket>(service);
+            }
             _doAccept();
         }
 
+        ~Server() {
+            printf("Server::~Server");
+        }
     private:
-        void _doAccept()
-        {
-            _acceptor.async_accept(_socket, [this](std::error_code ec) {
-                if (!ec)
-                {
-                    Session *s = new (std::nothrow) Session(std::move(_socket), [this](Session *s, const char *data, size_t length) {
-                        if (data != nullptr)
-                        {
-                            _mutex.lock();
-                            std::for_each(_sessions.begin(), _sessions.end(), [data, length](Session *s) {
-                                s->deliver(data, length);
-                            });
-                            _mutex.unlock();
-                        }
-                        else
-                        {
-                            _mutex.lock();
-                            _sessions.erase(s);
-                            _mutex.unlock();
-                            delete s;
-                        }
-                    });
-                    if (s != nullptr)
-                    {
+        void _doAccept() {
+            for (size_t i = 0; i < max_accept; ++i) {
+                _acceptor.async_accept(*_socket[i], std::bind(&Server::_acceptCallback, this, i, std::placeholders::_1));
+            }
+        }
+
+        void _acceptCallback(size_t index, std::error_code ec) {
+            if (!ec) {
+                Session *s = new (std::nothrow) Session(std::move(*_socket[index]), [this](Session *s, const char *data, size_t length) {
+                    if (data != nullptr) {
                         _mutex.lock();
-                        _sessions.insert(s);
+                        std::for_each(_sessions.begin(), _sessions.end(), [data, length](Session *s) {
+                            s->deliver(data, length);
+                        });
                         _mutex.unlock();
-                        s->start();
+                    } else {
+                        _mutex.lock();
+                        _sessions.erase(s);
+                        _mutex.unlock();
+                        delete s;
                     }
+                });
+                if (s != nullptr) {
+                    _mutex.lock();
+                    _sessions.insert(s);
+                    _mutex.unlock();
+                    s->start();
                 }
-                _doAccept();
-            });
+            }
+            _acceptor.async_accept(*_socket[index], std::bind(&Server::_acceptCallback, this, index, std::placeholders::_1));
         }
 
         asio::ip::tcp::acceptor _acceptor;
-        asio::ip::tcp::socket _socket;
+
+        enum { max_accept = 128 };
+        std::shared_ptr<asio::ip::tcp::socket> _socket[max_accept];
 
         std::unordered_set<Session *> _sessions;
         std::mutex _mutex;
@@ -224,27 +200,27 @@ int main(int argc, char *argv[])
         asio::io_service service;
         //echo::Server s(service, 8899);
         chat::Server s(service, 8899);
+        asio::io_service::work work(service);
 
         SYSTEM_INFO systemInfo;
         ::GetSystemInfo(&systemInfo);
         DWORD workerThreadCnt = systemInfo.dwNumberOfProcessors * 2 + 2;
         std::vector<std::thread *> workerThreads;
-        for (DWORD i = 0; i < workerThreadCnt; ++i)
-        {
+        for (DWORD i = 0; i < workerThreadCnt; ++i) {
             workerThreads.push_back(new (std::nothrow) std::thread([&service](){
                 service.run();
             }));
         }
 
-        while (scanf("%*c") != EOF)
-        {
+        while (scanf("%*c") != EOF) {
             continue;
         }
         service.stop();
 
-        for (DWORD i = 0; i < workerThreadCnt; ++i)
-        {
-            workerThreads[i]->join();
+        for (DWORD i = 0; i < workerThreadCnt; ++i) {
+            if (workerThreads[i]->joinable()) {
+                workerThreads[i]->join();
+            }
             delete workerThreads[i];
         }
     }
