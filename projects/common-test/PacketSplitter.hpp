711f82173ee93d52e6add884a6d3990355bbd95f
Jeff Wang <wohaaitinciu@163.com>
2015-08-01 17:29:06 +0800

    增加一些函数

--- projects/common-test/PacketSplitter.hpp
+++ projects/common-test/PacketSplitter.hpp
@@ -12,15 +12,17 @@ namespace jw {
         std::vector<char> _cacheBuf;
 
     public:
+        // 解出的包从buf中传回
         void decodeRecvPacket(std::vector<char> &buf, const char *data, size_t length) {
-            std::copy(data, data + length, std::back_inserter(_cacheBuf));
+            std::copy(data, data + length, std::back_inserter(_cacheBuf));  // 将data复制到cache尾部
             size_t bufSize = _cacheBuf.size();
-            if (bufSize < 4) {
-                LOG_DEBUG("包头不够");
+            if (bufSize < 4) {  // 包头
+                LOG_DEBUG("not enough for packet header");
                 buf.clear();
                 return;
             }
 
+            // 计算包体长度
             size_t recvLen = (unsigned char)_cacheBuf[3];
             recvLen <<= 8;
             recvLen |= (unsigned char)_cacheBuf[2];
@@ -29,30 +31,42 @@ namespace jw {
             recvLen <<= 8;
             recvLen |= (unsigned char)_cacheBuf[0];
             if (bufSize - 4 < recvLen) {
-                LOG_DEBUG("包体不够，目标%lu", (unsigned long)recvLen);
+                LOG_DEBUG("not enough for packet body, expect : %lu", (unsigned long)recvLen);
                 buf.clear();
                 return;
             }
 
-            buf = std::move(_cacheBuf);
+            buf = std::move(_cacheBuf);  // 移动到buf里面
             if (bufSize - 4 > recvLen) {
+                // 将多余的保存在cache里面，并从buf中移走多余的
                 std::copy(buf.begin() + 4 + recvLen, buf.end(), std::back_inserter(_cacheBuf));
                 buf.erase(buf.begin() + 4 + recvLen, buf.end());
-                LOG_DEBUG("包体太长，余下%lu", (unsigned long)bufSize - 4 - recvLen);
+                LOG_DEBUG("to long for packet body, remain : %lu", (unsigned long)bufSize - 4 - recvLen);
             }
             LOG_DEBUG("%.*s", (int)buf.size() - 4, &buf[4]);
         }
 
+        std::vector<char> decodeRecvPacket(const char *data, size_t length) {
+            std::vector<char> buf;
+            decodeRecvPacket(buf, data, length);
+            return std::move(buf);
+        }
 
         void encodeSendPacket(std::vector<char> &buf, const std::string &str) {
             buf.clear();
-            size_t sendLen = str.length();
+            size_t sendLen = str.length();  // 包体长度
             buf.reserve(sendLen + 4);
             buf.push_back((sendLen >> 0) & 0xFF);
             buf.push_back((sendLen >> 8) & 0xFF);
             buf.push_back((sendLen >> 16) & 0xFF);
             buf.push_back((sendLen >> 24) & 0xFF);
             std::copy(str.begin(), str.end(), std::back_inserter(buf));
+        }
+
+        std::vector<char> encodeSendPacket(const std::string &str) {
+            std::vector<char> buf;
+            encodeSendPacket(buf, str);
+            return buf;
         }
     };
 
@@ -68,10 +82,22 @@ namespace jw {
             }
         }
 
+        jw::cppJSON decodeRecvPacket(const char *data, size_t length) {
+            jw::cppJSON ret;
+            decodeRecvPacket(ret, data, length);
+            return ret;
+        }
+
         void encodeSendPacket(std::vector<char> &buf, const jw::cppJSON &json) {
             std::string str = json.PrintUnformatted();
             LOG_DEBUG("%s", str.c_str());
-            return PacketSplitter::encodeSendPacket(buf, str);
+            PacketSplitter::encodeSendPacket(buf, str);
+        }
+
+        std::vector<char> encodeSendPacket(const jw::cppJSON &json) {
+            std::vector<char> buf;
+            encodeSendPacket(buf, json);
+            return buf;
         }
     };
 }
